3d776280bd59f57a02ae088c72766b9102bec069
Ian July <ianjuly4@gmail.com>
2024-08-09 15:47:06 -0600

    authenticating users lab

--- server/app.py
+++ server/app.py
@@ -47,11 +47,39 @@ class ShowArticle(Resource):
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
+    
+class Login(Resource):
 
+    def post(self):
+        user = User.query.filter(
+            User.username == request.get_json()['username']
+        ).first()
+
+        session['user_id'] = user.id
+        return user.to_dict()
+
+class Logout(Resource):
+
+    def delete(self):
+        session.pop('user_id', None)
+        return {'message': '204: No Content'}, 204
+    
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return user.to_dict(), 200
+        else:
+            response = make_response(jsonify({}), 401)
+            response.headers['Content-Type'] = 'application/json'
+        return response
+    
+api.add_resource(CheckSession, '/check_session')
+api.add_resource(Logout, '/logout')
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
-
+api.add_resource(Login, '/login')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
